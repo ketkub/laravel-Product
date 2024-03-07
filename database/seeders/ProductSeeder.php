067ddbb5cf430895045705ca8a3cd933b57048d4
unknown <ketwowwow@gmail.com>
2024-03-07 15:28:16 +0700

    first commit

---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,48 @@
+<?php
+  
+namespace Database\Seeders;
+  
+use Illuminate\Database\Seeder;
+use App\Models\Product;
+  
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $products = [
+            [
+                'name' => 'Samsung Galaxy',
+                'description' => 'Samsung Brand',
+                'image' => 'https://th.bing.com/th/id/R.644375e059f8769e1fd5ea820dd8a44a?rik=7xNIAfcbFogYQg&pid=ImgRaw&r=0',
+                'price' => 100
+            ],
+            [
+                'name' => 'Apple iPhone 12',
+                'description' => 'Apple Brand',
+                'image' => 'https://dummyimage.com/200x300/000/fff&text=Iphone',
+                'price' => 500
+            ],
+            [
+                'name' => 'Google Pixel 2 XL',
+                'description' => 'Google Pixel Brand',
+                'image' => 'https://dummyimage.com/200x300/000/fff&text=Google',
+                'price' => 400
+            ],
+            [
+                'name' => 'LG V10 H800',
+                'description' => 'LG Brand',
+                'image' => 'https://dummyimage.com/200x300/000/fff&text=LG',
+                'price' => 200
+            ]
+        ];
+  
+        foreach ($products as $key => $value) {
+            Product::create($value);
+        }
+    }
+}
